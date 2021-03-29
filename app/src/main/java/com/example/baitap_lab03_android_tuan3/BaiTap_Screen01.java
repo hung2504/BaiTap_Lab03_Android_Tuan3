b4ab44f541e52d12b4e73d34d383d09c7121842f
hung2504 <vanhung25042000@gmail.com>
2021-03-29 08:46:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitap_lab03_android_tuan3/BaiTap_Screen01.java
@@ -0,0 +1,14 @@
+package com.example.baitap_lab03_android_tuan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaiTap_Screen01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baitap_screen01);
+    }
+}
